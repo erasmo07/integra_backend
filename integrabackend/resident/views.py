2ca2a6f8e3b73883a150d87bae0a77e1b812acd2
erasmo <erasmomontes07@gmail.com>
2018-10-19 09:07:36 -0400

    Add new entity call Person.

--- integrabackend/resident/views.py
+++ integrabackend/resident/views.py
@@ -1,12 +1,20 @@
 from rest_framework import viewsets, mixins
-from .models import Resident
-from .serializers import ResidentSerializer
+
+from .models import Resident, Person
+from .serializers import ResidentSerializer, PersonSerializer
 
 
-class ResidentCreateViewSet(mixins.CreateModelMixin,
-                            viewsets.GenericViewSet):
+class ResidentCreateViewSet(viewsets.ModelViewSet):
     """
     Create resident
     """
     queryset = Resident.objects.all()
     serializer_class = ResidentSerializer
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    """
+    Create resident
+    """
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
