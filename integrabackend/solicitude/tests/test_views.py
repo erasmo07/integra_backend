ad364b9fa7a48153d7154e6c6a3cf11d92d64afe
erasmo <erasmomontes07@gmail.com>
2018-11-12 17:12:32 -0400

    Add PUT method on aviso endpoint.

--- integrabackend/solicitude/tests/test_views.py
+++ integrabackend/solicitude/tests/test_views.py
@@ -85,3 +85,84 @@ class TestSolicitudeServiceTestCase(APITestCase):
         eq_(service.get('phone'), service_request.phone)
         eq_(service.get('email'), service_request.email)
         eq_(service.get('ownership'), service_request.ownership)
+
+
+class TestAvisoTestCase(APITestCase):
+    """
+    Test /aviso endpoint
+    """
+    def setUp(self):
+        self.base_name = 'create_aviso'
+        self.url = reverse('%s-list' % self.base_name)
+        self.client.force_authenticate(user=UserFactory.build())
+        return super(TestAvisoTestCase, self).setUp()
+    
+    def test_request_post_without_ticket_id(self):
+        body = {'ticket_id': ''} 
+
+        response = self.client.post(self.url, data=body)
+        eq_(response.status_code, status.HTTP_404_NOT_FOUND)
+        eq_(response.content, b'{"message":"Not set ticket_id"}')
+    
+    def test_request_post_service_request_not_exists(self):
+        body = {'ticket_id': 1} 
+
+        response = self.client.post(self.url, data=body)
+        eq_(response.status_code, status.HTTP_404_NOT_FOUND)
+        eq_(response.content,
+            b'{"message":"Not exists service request with it ticket_id"}')
+    
+    def test_request_get_without_ticket_id(self):
+        response = self.client.get(self.url)
+        eq_(response.status_code, status.HTTP_404_NOT_FOUND)
+        eq_(response.content, b'{"message":"Not set ticket_id"}')       
+    
+    def test_request_get_service_request_not_exists(self):
+        body = {'ticket_id': 1} 
+
+        response = self.client.get(self.url, data=body)
+        eq_(response.status_code, status.HTTP_404_NOT_FOUND)
+        eq_(response.content,
+            b'{"message":"Not exists service request with it ticket_id"}')
+    
+    def test_request_get_success(self):
+        property = PropertyFactory(
+            property_type=PropertyTypeFactory.create())
+        date_service_request = DateServiceRequestFactory()
+        day_type = DayTypeFactory()
+        day = DayFactory(day_type=day_type)
+        date_service_request.day.add(day)
+        service_request = ServiceRequestFactory(
+            service=ServiceFactory.create(),
+            state=StateFactory.create(),
+            user=UserFactory.create(), 
+            property=property,
+            date_service_request=date_service_request,
+            ticket_id=1, aviso_id=1)
+
+        body = {'ticket_id': 1} 
+        response = self.client.get(self.url, data=body)
+        eq_(response.status_code, status.HTTP_200_OK)
+    
+    def test_request_patch_success(self):
+        property = PropertyFactory(
+            property_type=PropertyTypeFactory.create())
+        date_service_request = DateServiceRequestFactory()
+        day_type = DayTypeFactory()
+        day = DayFactory(day_type=day_type)
+        date_service_request.day.add(day)
+        service_request = ServiceRequestFactory(
+            service=ServiceFactory.create(),
+            state=StateFactory.create(),
+            user=UserFactory.create(), 
+            property=property,
+            date_service_request=date_service_request,
+            ticket_id=1, aviso_id=1)
+
+        body = {'state': 'RACU'} 
+        url = reverse("%s-detail" % self.base_name, kwargs={"pk": 1})
+        response = self.client.put(url, data=body)
+
+        eq_(response.status_code, status.HTTP_200_OK)
+        ok_(response.json().get('success'))
+        eq_(len(mail.outbox), 1)
