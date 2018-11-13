2d604d22ebef7f83ef8e1cbff00cdf038e8f0912
erasmo <erasmomontes07@gmail.com>
2018-11-13 10:22:16 -0400

    Add configuration to send emails from application

--- integrabackend/config/common.py
+++ integrabackend/config/common.py
@@ -51,6 +51,9 @@ class Common(Configuration):
 
     # Email
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'owa.puntacana.com'
+    DEFAULT_FROM_EMAIL = "no-reply@puntacana.com"
+    EMAIL_PORT = 25
 
     ADMINS = (
         ('Author', 'aplicaciones@puntacana.com'),
