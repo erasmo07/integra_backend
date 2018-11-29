425db1dd906cf9bf850ad5f0807e7fc064486e50
erasmo <erasmomontes07@gmail.com>
2018-11-29 01:35:47 -0400

    Add variable configuration for celery on common.py file.

--- integrabackend/config/common.py
+++ integrabackend/config/common.py
@@ -212,3 +212,11 @@ class Common(Configuration):
     }
 
     CORS_ORIGIN_ALLOW_ALL = True
+
+    # CELERY STUFF
+    BROKER_URL = 'redis://redis:6379'
+    CELERY_RESULT_BACKEND = 'redis://redis:6379'
+    CELERY_ACCEPT_CONTENT = ['application/json']
+    CELERY_TASK_SERIALIZER = 'json'
+    CELERY_RESULT_SERIALIZER = 'json'
+    CELERY_TIMEZONE = 'Africa/Nairobi'
