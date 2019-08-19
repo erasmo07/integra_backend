7ab258d8b67af8ad41b7d3e9e8e433bcd6f53ed9
erasmo <erasmomontes07@gmail.com>
2019-08-19 15:20:06 -0400

    PORTAL-31 Add assign task to load services

--- integrabackend/solicitude/tasks.py
+++ integrabackend/solicitude/tasks.py
@@ -3,6 +3,8 @@ from celery.utils.log import get_task_logger
 from integrabackend.celery import app
 
 from integrabackend.solicitude import models, helpers
+from integrabackend.resident.models import ProjectService, Department
+from partenon.helpdesk import Topics
 
 
 logger = get_task_logger(__name__)
@@ -16,3 +18,23 @@ def create_service_request(service_request_id):
 
     logger.info("Creando solicitud de servicio en sistemas externos")
     helpers.create_service_request(service_request)
+
+
+@app.task(name='create_service_from_faveo')
+def create_services_from_faveo():
+    logger.info("Buscando los helptopic de faveo")
+    for topic in Topics.objects.get_entitys():
+        service, create = models.Service.objects.get_or_create(
+            name=topic.topic, sap_code_service='S4')
+        logger.info(f"Helptopic {service.name} was create {create}")
+
+        department = Department.objects.filter(fave_id=topic.department)
+        if not department.exists():
+            continue
+
+        logger.info(f"Relating {service.name} with {department.first().name}")
+        project_service = ProjectService.objects.filter(
+            project__department=department.first())
+        if project_service.exists():
+            project_service.first().services.add(service)
+            logger.info(f"Related {service.name} with {department.first().name}")
