ceb2d08d03c35e976041a6efb48b25a6ea68f195
Felix De Gracia <pitrafa@gmail.com>
2019-09-03 11:23:42 -0400

    Test permission class returns the correct each test case.

---
+++ integrabackend/solicitude/tests/permissions/test_permission_class.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from ...permissions import HasCreditPermission
+from mock import patch, MagicMock
+from django.test import tag
+from ....users.test.factories import UserFactory
+from ....resident.test.factories import ResidentFactory
+
+
+class HasCreditPermissionTest(TestCase):
+
+    def setUp(self):
+        self.permission = HasCreditPermission()
+        self.view = MagicMock()
+
+    def get_request(self, sap_customer):
+        user = UserFactory.create()
+        ResidentFactory(
+            user=user, sap_customer=sap_customer)
+        request = MagicMock(user=user)
+        return request
+
+    @tag('integration')
+    def test_permission_class_returns_true_if_resident_has_credit(self):
+        # Arrange
+        request = self.get_request(2935)
+
+        # Act and Assert
+        self.assertTrue(self.permission.has_permission(request, self.view))
+
+    @tag('integration')
+    def test_permission_class_returns_false_if_resident_has_not_credit(self):
+        # Arrange
+        request = self.get_request(4635)
+
+        # Act and Assert
+        self.assertFalse(self.permission.has_permission(request, self.view))
+
+    @tag('unit')
+    @patch('partenon.ERP.ERPClient.has_credit')
+    def test_permission_class_returns_true_if_resident_has_credit_unit(self, mock_has_credit):
+        # Arrange
+        mock_has_credit.return_value = {
+            "puede_consumir": True
+        }
+        request = self.get_request(2935)
+
+        # Act and Assert
+        self.assertTrue(self.permission.has_permission(request, self.view))
+
+    @tag('unit')
+    @patch('partenon.ERP.ERPClient.has_credit')
+    def test_permission_class_returns_false_if_resident_has_no_credit_unit(self, mock_has_credit):
+        # Arrange
+        mock_has_credit.return_value = {
+            "puede_consumir": False
+        }
+        request = self.get_request(2935)
+
+        # Act and Assert
+        self.assertFalse(self.permission.has_permission(request, self.view))
