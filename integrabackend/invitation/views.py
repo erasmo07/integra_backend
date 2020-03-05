b8722376c5ee079391d030a0e879f4e6f0bd2785
Erasmo Montes <erasmomontes07@gmail.com>
2020-03-05 20:41:23 +0000

    APP-93 Remove import filters in views.
    Approved-by: Erasmo Montes

--- integrabackend/invitation/views.py
+++ integrabackend/invitation/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 
-from . import models, serializers, mixins, filters
+from . import models, serializers, mixins
 from ..resident.models import Property
 
 
