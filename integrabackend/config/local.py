4135094c499f2361a0f30bc77128dbb1ba74de10
erasmo <erasmomontes07@gmail.com>
2020-04-23 14:48:14 -0400

    Fixed email to password reset email.

--- integrabackend/config/local.py
+++ integrabackend/config/local.py
@@ -1,5 +1,8 @@
 import os
+import sentry_sdk
 from .common import Common
+from sentry_sdk.integrations.django import DjangoIntegration
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -7,19 +10,29 @@ class Local(Common):
     DEBUG = True
 
     # Testing
+    if not DEBUG:
+        sentry_sdk.init(
+            dsn="https://0f2c75c7488b4afe90ff95e5c426f889@o82137.ingest.sentry.io/1538689",
+            integrations=[DjangoIntegration()],
+
+            # If you wish to associate users to errors (assuming you are using
+            # django.contrib.auth) you may enable sending PII data.
+            send_default_pii=True
+        )
+
     INSTALLED_APPS = Common.INSTALLED_APPS
     INSTALLED_APPS += ('django_nose',)
     TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-    NOSE_ARGS = [
-        BASE_DIR,
-        '-s',
-        '--nologcapture',
-        '--with-coverage',
-        '--with-progressive',
-        '--cover-package=integrabackend'
-    ]
+    NOSE_ARGS = ['-s',]
 
     # Mail
-    EMAIL_HOST = 'localhost'
-    EMAIL_PORT = 1025
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    # EMAIL_HOST = 'localhost'
+    # EMAIL_PORT = 1025
+    # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': 'mydatabase',
+    #     }
+    # }
